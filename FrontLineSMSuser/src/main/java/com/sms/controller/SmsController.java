7fc0cf0ab0200bbed9afda069eb6cf95dbf23ec7
datati <datati@L-DATATI-1.techflow.com>
2014-11-18 00:53:11 -0800

    added the json objects

--- FrontLineSMSuser/src/main/java/com/sms/controller/SmsController.java
+++ FrontLineSMSuser/src/main/java/com/sms/controller/SmsController.java
@@ -2,22 +2,18 @@ package com.sms.controller;
 
 import java.io.IOException;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.github.kevinsawicki.http.HttpRequest;
 
 /**
  * @author Douglas O Atati
@@ -31,49 +27,40 @@ public class SmsController {
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String makeJson(Model model) throws IOException {
-		
-		//TO DO:this is for storing the form properties into a json object
+
+		// TO DO:Will make this is JSON object from form properties given by the user
 		JSONObject obj = new JSONObject();
-		obj.put("name", "dee");
-		obj.put("age", new Integer(100));
+		obj.put("secret","kingslanding");
+		obj.put("message", "Hello, Douglas. Servers are down!");
+		JSONObject obj8 = new JSONObject();
+		obj8.put("type", "address");
+		obj8.put("value", "1234567890");
 		JSONArray list = new JSONArray();
-		list.add("msg 1");
-		list.add("msg 2");
-		list.add("msg 3");
-		obj.put("messages", list);
-		JSONObject json = new JSONObject();
-		json.put("someKey", "someValue");
-		//test string to json
-		String jsonStr = "{ 'secret':'kingslanding',"
-				+ "'message':'Hello, Doug. The servers are down.',"
-				+ "'recipients':[{ 'type':'group', '':'' },"
-				+ "{ 'type':'group', 'name':'friends' },"
-				+ "{ 'type':'smartgroup', 'id':'' },"
-				+ "{ 'type':'smartgroup', 'name':''},"
-				+"{ 'type':'contact', 'id':'' },"
-				+"{ 'type':'contact', 'name':'' },"
-				+"{ 'type':'address','value':'+1234567890'}]}";
+		obj.put("recipients", list);
 		
-		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+		//Perform a GET request and get the status of the response. This uses the kevinsawicki maven plugin.This works..
+		int response = HttpRequest.get("https://cloud.frontlinesms.com/api/1/smssync/2762/kingslanding?task=send&messages=Hello+World+response&recipients=12027019440").code();
+		System.out.println("Doug your Get Response was :" + response);
 		
-		try {
-			HttpPost request = new HttpPost("https://cloud.frontlinesms.com/api/1/smssync/2762/kingslanding");
-			StringEntity params = new StringEntity(jsonStr.toString());
+		//POST json to https and read response. Getting a 500 error on this:(
+		CloseableHttpClient httpClient = HttpClientBuilder.create().build(); 
+		try { 
+			HttpPost request = new HttpPost("https://cloud.frontlinesms.com/api/1/smssync/2762/kingslanding"); 
+			StringEntity params = new StringEntity(obj.toJSONString());
 			request.addHeader("content-type", "application/json");
-			request.setEntity(params);
-			HttpResponse response = httpClient.execute(request);
-			
-			//System.out.println("Doug your Response was :" + response);
+			request.setEntity(params); 
+			HttpResponse postresponse =httpClient.execute(request);
 
 			// handle response here...
-		} catch (Exception ex) {
-			// handle exception here
-		} finally {
-			httpClient.close();
-		}
+			System.out.println("Doug your Post Response was :" + postresponse);
 
+		} catch (Exception ex) { 
+			// handle exception here 
+			} 
+		finally {
+			httpClient.close(); 
+		}
 		model.addAttribute("messages", obj);
-		
 		return "helloWorld";
 	}
 
